f7d659ce8d3912b12acd48ffbfb8d1ca5b39189d
qiufeng001 <2227052723@qq.com>
2018-12-13 17:08:20 +0800

    测试dubbo

--- src/main/java/wx/milk/api/server/IAdministratorDubboProvider.java
+++ src/main/java/wx/milk/api/server/IAdministratorDubboProvider.java
@@ -1,17 +1,10 @@
 package wx.milk.api.server;
 
-
-import wx.redis.WxJedisCommands;
-
 /**
  * 用于提供用户基本操作基本数据获取
  */
 public interface IAdministratorDubboProvider {
-
-    /**
-     * 获取用户模块的redis操作
-     */
-    WxJedisCommands getCommonJedis();
+    String getStr(String str);
 }
 
 
