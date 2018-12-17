658dfc5dc0b6dad1f92994c63bc383fe73414265
qiufeng001 <2227052723@qq.com>
2018-12-17 08:34:04 +0800

    测试dubbo

--- src/main/java/wx/milk/api/server/ITestDubboProvider.java
+++ src/main/java/wx/milk/api/server/ITestDubboProvider.java
@@ -3,8 +3,6 @@ package wx.milk.api.server;
 /**
  * 用于提供用户基本操作基本数据获取
  */
-public interface IAdministratorDubboProvider {
+public interface ITestDubboProvider {
     String getStr(String str);
 }
-
-
